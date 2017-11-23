fbcfaf4d500568d20de3f394a9d8fe3b46d180ad
Dustin Essington <aetaric@gmail.com>
2017-11-23 09:38:33 -0500

    added 'version' to botinfo command

--- bot.rb
+++ bot.rb
@@ -81,6 +81,7 @@ end
 $bot.command(:botinfo, bucket: :general, rate_limit_message: 'Calm down for %time% more seconds!') do |event|
   shard_value = (shard + 1).to_s + "/" + total_shards.to_s
   seconds = `ps -o etimes= -p '#{Process.pid}'`.strip.to_i
+  rev = `git rev-parse HEAD`.strip.split(//).first(9).join("")
   event.channel.send_embed do |embed|
     embed.title = "Ghost"
     embed.description = "A discord bot for Destiny 2 clans"
@@ -90,6 +91,7 @@ $bot.command(:botinfo, bucket: :general, rate_limit_message: 'Calm down for %tim
     embed.add_field(name: "Servers", value: $bot.servers.count, inline: true)
     embed.add_field(name: "Shard", value: shard_value, inline: true)
     embed.add_field(name: "Uptime", value: humanize(seconds), inline: true)
+    embed.add_field(name: "Version", value: rev, inline: true)
     embed.add_field(name: "Library", value: "discordrb", inline: true)
     embed.add_field(name: "Support Server", value: "https://discord.gg/8My2HqS", inline: true)
   end
