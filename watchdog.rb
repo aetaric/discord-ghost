68bded6ac1dece2ba9f463044dca716e8b05e13b
Dustin Essington <aetaric@gmail.com>
2017-11-25 12:31:32 -0500

    Continually try to keep shards active.

--- watchdog.rb
+++ watchdog.rb
@@ -4,6 +4,9 @@ require 'json'
 
 def shut_down
   puts "\nShutting down gracefully..."
+  threads.each do |thread|
+    thread.exit
+  end
   $pids.each do |pid|
     Process.kill("INT", pid)
   end
@@ -34,9 +37,11 @@ $pids = []
 config["shards"].times do |shard|
   # find abstract way to spawn these so we can still kill them safely
   threads << Thread.new do
-    pid = Process.spawn({ "SHARD" => shard.to_s, "TOTALSHARDS" => config["shards"].to_s }, "ruby bot.rb", [:out, :err]=>["logs/log_shard" + shard.to_s, "w"])
-    $pids.push pid
-    Process.wait(pid)
+    while true do
+      pid = Process.spawn({ "SHARD" => shard.to_s, "TOTALSHARDS" => config["shards"].to_s }, "ruby bot.rb", [:out, :err]=>["logs/log_shard" + shard.to_s, "w"])
+      $pids.push pid
+      Process.wait(pid)
+    end
   end
 end
 
