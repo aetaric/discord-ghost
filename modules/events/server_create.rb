f080bde46d05282ba555184200d2674fd4e0d8a0
Dustin Essington <aetaric@gmail.com>
2017-12-06 22:44:44 -0500

    reorder code to clean up broken servers on join

--- modules/events/server_create.rb
+++ modules/events/server_create.rb
@@ -5,6 +5,10 @@ module Ghost
       
       server_create do |event|
         puts "Ghost was added to server: #{event.server.name} (#{event.server.id})"
+
+        statement = $mysql.prepare("INSERT INTO servers (sid,created_at,updated_at) VALUES (?,NOW(),NOW())")
+        result = statement.execute(event.server.id.to_s)
+
         bot_member = $bot.profile.on(event.server)
         event.server.channels.each do |channel|
           if bot_member.permission?(:send_messages, channel)
@@ -17,9 +21,6 @@ Please take a moment to run the !configure *guild id* command in a text channel 
             puts "No permission in channel: #{channel.name}"
           end
         end
-        statement = $mysql.prepare("INSERT INTO servers (sid,created_at,updated_at) VALUES (?,NOW(),NOW())")
-        result = statement.execute(event.server.id.to_s)
-
       end
     end
   end
