08b92721169fc2692c2df8c61485983675a4fc95
Dustin Essington <aetaric@gmail.com>
2017-12-10 10:41:33 -0500

    updated usage info for configure command

--- modules/commands/configure.rb
+++ modules/commands/configure.rb
@@ -4,7 +4,7 @@ module Ghost
       extend Discordrb::Commands::CommandContainer
 
       command(:configure, bucket: :general, rate_limit_message: 'Calm down for %time% more seconds!', required_permissions: [:administrator], min_args: 1, max_args: 1, 
-              description: "Configures the bot to use your Destiny 2 guild for guild-related commands on this server.", usage: "!configure 1234567", 
+              description: "Configures the bot to use your Destiny 2 guild for guild-related commands on this server.", usage: "!configure destiny2_clan_id", 
               permission_message: "Guardian, You need permission from the vanguard to do this! (User lacks defined role with \"Administrator\" set)") do |event,guild_id|
         if guild_id.nil?
           event.send_message "Guardian, I need the guild ID. Please provide it as an arguement to the command."
