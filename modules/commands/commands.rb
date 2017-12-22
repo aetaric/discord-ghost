b6c837e3f1f77dffa5e693f05c869faa1e5355b2
Dustin Essington <aetaric@gmail.com>
2017-12-21 19:00:28 -0500

    update command list.

--- modules/commands/commands.rb
+++ modules/commands/commands.rb
@@ -5,12 +5,17 @@ module Ghost
 
       command(:commands, bucket: :general, rate_limit_message: 'Calm down for %time% more seconds!', description: "Lists all the commands and a description.") do |event|
         event.send_temporary_message "```
+      !help        - Displays the command list as well as provides usage information if needed.
       !botinfo     - Displays bot statistics and info.
+      !configure   - Configures the bot for your clan.
       !item        - Searches for the item passed after the command.
       !nightfall   - Pulls the realtime info about the current nightfall.
+      !leviathan   - Displays the rotation of the Leviathan Raid.
+      !xur         - Displays the items Xur has for sale. If he's not around. Displays the time he will be back.
+      !eververse   - Displays the items Tess has for sale.
       !newschannel - Sets the channel this is run in as your news channel. Any posts from Bungie's blog or @BungieHelp on twitter will be linked in this channel.
-      !claninfo    - Pulls the realtime info about your clan.
-      !engrams     - Pulls the realtime info about the clan engrams.```", 60.to_f
+      !claninfo    - Pulls realtime info about your clan.
+      !engrams     - Pulls your clan's engrams.```", 60.to_f
         temp_timers = Timers::Group.new
         temp_timers.after(60) { event.message.delete }
         temp_timers.wait
